2c35cec27ae7b038c7755059240b0fa41bb000d2
JON <jonn.thinkrtc@gmail.com>
2019-01-07 09:01:46 -0500

    register superuser

--- todo/admin.py
+++ todo/admin.py
@@ -6,5 +6,4 @@ from todo.models import ToDo
 class ToDoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
-
-admin.site.register(ToDoAdmin,ToDo)
+admin.site.register(ToDo, ToDoAdmin)
